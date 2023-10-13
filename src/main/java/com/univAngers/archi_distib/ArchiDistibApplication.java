22ff96c115528e5951e7372017fdd531fda9ee8e
lacostam <jean44774@gmail.com>
2023-10-13 10:05:02 +0200

    first

---
+++ src/main/java/com/univAngers/archi_distib/ArchiDistibApplication.java
@@ -0,0 +1,13 @@
+package com.univAngers.archi_distib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiDistibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchiDistibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/univAngers/archi_distib/ArchiDistibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univAngers.archi_distib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchiDistibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
