af32c37233b484a5defca8ebfe8c958251e7af71
lacostam <jean44774@gmail.com>
2023-11-14 14:39:41 +0100

    ajout du InternalLeadDto

--- internalThrift/src/main/thrift/Customer.thrift
+++ internalThrift/src/main/thrift/Customer.thrift
@@ -1,4 +1,4 @@
-namespace java com.univAngers.archi_distrib
+namespace java InternalCRMService
 
 struct LeadTO {
   1: required string firstName,
@@ -14,9 +14,26 @@ struct LeadTO {
   11: required string state
 }
 
+struct InternalLeadDto {
+  1: required string name,
+  2: optional double annualRevenue,
+  3: required string phone,
+  4: required string street,
+  5: required string postalCode,
+  6: required string city,
+  7: required string country,
+  8: required i64 creationDate,
+  9: required string company,
+  10: required string state,
+}
+
 service InternalCRMService {
+  void ping(),
+
   void addLead(1: LeadTO lead),
   list<LeadTO> findLeads(1: double lowAnnualRevenue, 2: double highAnnualRevenue, 3: string state),
   list<LeadTO> findLeadsByDate(1: string startDate, 2: string endDate),
-  void deleteLead(1: LeadTO lead)
+  void deleteLead(1: LeadTO lead),
+
+  oneway void zip()
 }
